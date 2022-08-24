5d83a320107cbd10eaaef8051c419eda60ad6446
wooni005 <arjan@wooning.cz>
2022-08-24 10:53:32 +0200

    pyCharm inspection/PEP8 corrections

--- watchdog_kick.py
+++ watchdog_kick.py
@@ -4,13 +4,13 @@ import time
 import os
 from watchdogdev import watchdog
 
-exit = False
+exitThread = False
 
 os.nice(20)
 time.sleep(20)                  # Wait before starting
 wd = watchdog('/dev/watchdog')
 
-while not exit:
+while not exitThread:
     wd.keep_alive()
     # print ("Kick")
     time.sleep(5)
